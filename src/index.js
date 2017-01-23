cc35bcf4c9a2e917e853f4f0b29d0f0418de2445
Colin McNamara <coolinmc6@gmail.com>
2017-01-22 21:01:32 -0500

    Grider's API key update

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = '';
 
+// const API_KEY = '';
+const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
 
 
 class App extends Component  {
